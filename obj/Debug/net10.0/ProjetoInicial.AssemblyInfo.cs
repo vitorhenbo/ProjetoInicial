7cf28f785b8849f085b088fddd0ab485d4367fdf
Vitor <vitorhenriquebo@yahoo.com.br>
2025-04-04 16:51:55 -0300

    adiciona componente Mensagem à página de cadastro de produtos e implementa lógica de inclusão

--- obj/Debug/net10.0/ProjetoInicial.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProjetoInicial.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoInicial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4081cd357170b11b11efb59e80844456f0a3e1fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+767598b215a10dd2d41e4aac415ec36262e7261b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoInicial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoInicial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
